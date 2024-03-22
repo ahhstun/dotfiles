877683522ae00d70795f1bfe905f470f25436893
Austin <38300150+ahhstun@users.noreply.github.com>
2024-03-22 07:22:11 +0000

    added alacritty config and updated install.sh to use no-confirm

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-#install all linux packages used
-sudo pacman -S firefox obsidian alacritty htop npm neofetch neovim
+#install all linux packages useed
+#
 echo " Installing firefox obsidian alacritty htop npm neofetch neovim"
-sudo pacman -S python ripgrep spotify-launcher stow
-echo " Installing python ripgrep spotify-launcher stow"
+sudo pacman -S --noconfirm firefox alacritty htop npm neofetch neovim
+
+echo " Installing python ripgrep stow"
+
+sudo pacman -S python ripgrep  stow
 
 sudo pacman -Sg nerd-fonts-ttf-victor-mono-nerd
+
 echo "Nerd Fonts installed correctly"
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DOT_FOLDERS="bash,tmux,nvim,git"
+DOT_FOLDERS="bash,tmux,nvim,git,alacritty"
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     echo "[+] Folder :: $folder"
